dabfb47e04bb5af7ba280c685da2e51cc19aff5f
bugall <bugall@sina.cn>
2017-07-18 00:55:21 +0800

    LYNX-387- 暂时取消阶段检查的优化, 抽象所有的serivce为存在2级约束的结构

--- distribution/index.js
+++ distribution/index.js
@@ -8,6 +8,7 @@ const services = [{
     rate: 3.5,
     max_qty: 8,
     item_types: [{
+        _id: 'a8b3f31_1',
         name: '成人奶粉'
     }]
 }, {
@@ -17,6 +18,7 @@ const services = [{
     rate: 3.5,
     max_qty: 6,
     item_types: [{
+        _id: 'a8b3f32_1',
         name: '成人奶粉'
     }]
 }, {
@@ -24,6 +26,7 @@ const services = [{
     name: '奶粉BC线 - 3罐',
     type: 'bc',
     item_types: [{
+        _id: 'a8b3f33_1',
         name: '婴儿奶粉'
     }],
     fixed_rate: 15,
@@ -34,6 +37,7 @@ const services = [{
     name: '奶粉BC线 - 6罐',
     type: 'bc',
     item_types: [{ 
+        _id: 'a8b3f34_1',
         name: '婴儿奶粉'
     }],
     fixed_rate: 30,
@@ -44,11 +48,14 @@ const services = [{
     name: '杂物线',
     type: 'cc',
     item_types: [{
+        _id: 'a8b3f35_1',
         name: '保健品',
     }, {
+        _id: 'a8b3f35_2',
         name: '化妆品',
         max_qty: 2 // 包裹内最多2个
     }, {
+        _id: 'a8b3f35_3',
         name: '食品',
         max_qty: 3 // 包裹内最多3个，但是混搭其他类型，包裹内物品总数可以是8
     }],
@@ -62,6 +69,7 @@ const services = [{
     name: '首饰线',
     type: 'cc',
     item_types: [{
+        _id: 'a8b3f36_1',
         name: '首饰'
     }],
     rate: 6,
@@ -89,11 +97,12 @@ const orders = [{
 class Distribution {
     // TODO 2种模式： 'price' 价格优先, 'speed' 速度优先
     constructor(services, order) {
-        this.services = services;
-        this.order = order;
-        this.mode = 'speed';
-        this.productsInfo = [];
-        this.orderRecursiveInfo = {};
+        this.services = services; // 快递服务的配置
+        this.order = order; // 订单信息
+        this.mode = 'speed'; // 默认速度优先
+        this.productsInfo = []; // 商品的信息[商品与服务的关系]
+        this.orderRecursiveInfo = {}; // 保存订单里每个商品对应的快递的排列组合信息
+        this.orderExpressPackageInfo = {}; // 订单装包的情况, 每个快递包的使用情况
         this.finalAnswer= {};
     }
 
@@ -134,7 +143,7 @@ class Distribution {
             this.checkOrderServicesValid();
             return;
         }
-        
+
         const productId = this.order[productIndex]._id;
         this.generatProductCombination(productId, 0, this.productsInfo[productId].dependency.length, () => {
             // TODO 把check写完就ok了。
@@ -152,14 +161,34 @@ class Distribution {
     // 查看商品对应的services是否满足
     checkProductServicesValid(productId) {
         // 产品的排列组合数 {'1': 10, '2': 0}
-        const productRecursiveInfo = this.orderRecursiveInfo[productId];
-        const productDependency = this.productsInfo[productId].dependency;
-        const productInfo = _.forEach(this.order, (product) => product._id === productId)[0];
+        
+        return { success: serviceDistFlag, packageCounterInfo: packageCounterInfo };
+    }
+    caculCost() {
+
+    }
+    // 查看整个订单是否满足services
+    checkOrderServicesValid() {
+        console.log(this.orderRecursiveInfo);
+        const products = Object.keys(this.orderRecursiveInfo);
+        _.forEach(products, (productId) => {
+            this.takeProductToExpressPackage(productId);
+        });
+    }
+    takeProductToExpressPackage(productId) {
+        const productRecursiveInfo = this.orderRecursiveInfo[productId]; // 商品对应服务的排列组合信息
+        const productDependency = this.productsInfo[productId].dependency; // 商品对应的服务列表
+        const productInfo = _.forEach(this.order, (product) => product._id === productId)[0]; // 订单中商品的具体描述
         const packageCounterInfo = {};
         let serviceDistFlag = true;
+
         _.forEach(productRecursiveInfo, (value, index) => {
-            // 10个商品通过1号服务
+            // 找到快递服务的对应的具体信息
             const serviceInfo = _.filter(this.services, (service) => service._id === productDependency[Number(index - 1)])[0];
+            // init
+            if (_.isEmpty(this.orderExpressPackageInfo[serviceInfo._id])) {
+                this.initOrderExpressPackageByServiceId(serviceInfo._id);
+            }
             // 判断是否可以满足
             let productCounter = 0;
             let packageCounter = 1;
@@ -167,6 +196,7 @@ class Distribution {
                 weight: 0,
                 qty: 0,
             };
+            // TODO 暂时不考虑2级限制, 只考虑1级限制
             while (productCounter !== value) {
                 let allCheckFlag = 2;
                 // 开始一个个的装包
@@ -202,14 +232,26 @@ class Distribution {
                 packageCounterInfo[serviceInfo._id] = packageCounter;
             }
         });
-        return { success: serviceDistFlag, packageCounterInfo: packageCounterInfo };
     }
-    caculCost() {
+    initOrderExpressPackageByServiceId(serviceId) {
+        const initOpt = {
+            qty: 0,
+            weight: 0,
+        };
+        // 初始化一级约束的结构
+        this.orderExpressPackageInfo[serviceId] = {
+            topLimit: initOpt, 
+            secondLimit: {}
+        };
 
-    }
-    // 查看整个订单是否满足services
-    checkOrderServicesValid() {
-
+        // 初始化二级约束的结构
+        _.forEach(this.services, (service) => {
+            if (service._id === serviceId) {
+                _.forEach(service.item_types, (item) => {
+                    this.orderExpressPackageInfo[serviceId][item._id] = initOpt;
+                });
+            };
+        });
     }
     // 速度优先模式
     setSpeedPreferredMode() {
