6a8cda42efcb4d6cf476181b66630714b7a93259
bugall <bugall@sina.cn>
2017-07-20 02:02:02 +0800

    LYNX-387- 通过所有的测试, 可以review代码

--- test/test.js
+++ test/test.js
@@ -3,61 +3,68 @@ const should = require('chai').should(); // jshint ignore:line
 const _ = require('lodash');
 const services = require('./config').services;
 
-describe('顺丰API - 不拆包', () => {
-    // it('能够创建订单', () => {
-    //     const order = [{
-    //         _id: 'product_1abd1f3',
-    //         name: '德运奶粉',
-    //         weight: 1300,
-    //         type: '成人奶粉',
-    //         value: 8,
-    //         qty: 10
-    //     }, {
-    //         _id: 'product_2abd1f3',
-    //         name: 'Swisse维生素',
-    //         weight: 300,
-    //         type: '保健品',
-    //         value: 15,
-    //         qty: 10,
-    //     }];
-    //     const distrubution = new Distribution(services, order);
-    //     const { plan, cost } = distrubution.showMeTheAnswer();
-    //     console.log(plan , cost);
-    //     console.log(JSON.stringify(plan));
-    // });
+describe('分包', () => {
+    it('能够根据数量上线分包, 包裹重量包含包裹自身重量',  () => {
+        const order = [{
+            _id: 'product_1abd1f3',
+            name: '德运奶粉',
+            weight: 1300,
+            type: '成人奶粉',
+            value: 8,
+            qty: 14
+        }];
+        const distrubution = new Distribution(services, order);
+        const { plan, cost } = distrubution.showMeTheAnswer();
 
-    // it('能够创建订单', () => {
-    //     const order = [{
-    //         _id: 'product_3abd1f3',
-    //         name: '爱他美一段',
-    //         weight: 1300,
-    //         type: '婴儿奶粉',
-    //         value: 30,
-    //         qty: 3,
-    //     }, {
-    //         _id: 'product_4abd1f3',
-    //         name: '爱他美一段',
-    //         weight: 1300,
-    //         type: '婴儿奶粉',
-    //         value: 40,
-    //         qty: 4
-    //     }];
-    //     const distrubution = new Distribution(services, order);
-    //     const { plan, cost } = distrubution.showMeTheAnswer();
-    //     console.log(plan , cost);
-    // });
+        cost.should.equal(70); // 总费用
+        const expressKey = Object.keys(plan);
+        Object.keys(plan).length.should.equal(1); // 使用一个快递服务
+        plan[expressKey].length.should.equal(2); // 打两个包
+    });
+    it('能区分杂物与成人奶粉', () => {
+        const order = [{
+            _id: 'product_1abd1f3',
+            name: '德运奶粉',
+            weight: 1300,
+            type: '成人奶粉',
+            value: 8,
+            qty: 6
+        }, {
+            _id: 'product_2abd1f3',
+            name: 'Swisse维生素',
+            weight: 300,
+            type: '保健品',
+            value: 15,
+            qty: 2,
+        }];
+        const distrubution = new Distribution(services, order);
+        const { plan, cost } = distrubution.showMeTheAnswer();
+        // 总费用
+        cost.should.equal(43.5);
+        // 使用两个快递
+        Object.keys(plan).length.should.equal(2);
+        // 奶粉经济线 - 成人 1 个包裹
+        plan.express_1dad4fa.length.should.equal(1);
+        // 杂物线 1 个包裹
+        plan.express_5dad4fa.length.should.equal(1);
+    });
 
-    it('能够创建订单', () => {
+    it('能够识别包裹金额上限', () => {
         const order = [{
             _id: 'product_5abd1f3',
             name: '月光宝盒',
             weight: 200,
-            type: '首饰',
+            type: '化妆品',
             value: 100,
             qty: 3
         }];
         const distrubution = new Distribution(services, order);
         const { plan, cost } = distrubution.showMeTheAnswer();
-        console.log(plan , cost);
+        // 总费用
+        cost.should.equal(12);
+        // 使用1个快递
+        Object.keys(plan).length.should.equal(1);
+        // 杂物线2个包裹
+        plan.express_5dad4fa.length.should.equal(2);
     });
 });
