c78ae6dda78b258d3cc653c0b00aa05190c7e8e5
bugall <bugall@sina.cn>
2017-07-24 22:51:03 +0800

    LYNX-387- 增加速度优先的功能，修复几个bug，通过所有测试

--- test/config.js
+++ test/config.js
@@ -1,4 +1,4 @@
-const services = [{
+const costFirstService = [{
     _id: 'e_1000001',
     name: '奶粉经济线 - 成人',
     type: 'cc',
@@ -75,4 +75,103 @@ const services = [{
     max_qty: 3
 }];
 
-module.exports = { services };
+const speedFirstService = [{
+    _id: 'e_1000001',
+    name: '奶粉经济线 - 成人',
+    type: 'cc',
+    rate: 1,
+    max_qty: 8,
+    item_types: [{
+        _id: 'e_1000001_1',
+        name: '成人奶粉'
+    }]
+},{
+    _id: 'e_1000011',
+    name: '奶粉经济线 - 成人',
+    type: 'bc',
+    rate: 3.5,
+    max_qty: 8,
+    item_types: [{
+        _id: 'e_1000011_1',
+        name: '成人奶粉'
+    }]
+}, {
+    _id: 'e_1000002',
+    name: '奶粉经济线 - 婴儿',
+    type: 'bc',
+    rate: 3.5,
+    max_qty: 6,
+    item_types: [{
+        _id: 'e_1000002_1',
+        name: '婴儿奶粉'
+    }]
+}, {
+    _id: 'e_1000003',
+    name: '奶粉BC线 - 3罐',
+    type: 'bc',
+    item_types: [{
+        name: '婴儿奶粉',
+        _id: 'e_1000003_1',
+    }],
+    fixed_rate: 15,
+    min_qty: 3,
+    max_qty: 3
+}, {
+    _id: 'e_1000004',
+    name: '奶粉BC线 - 6罐',
+    type: 'bc',
+    item_types: [{
+        name: '婴儿奶粉',
+        _id: 'e_1000004_1',
+    }],
+    fixed_rate: 40,
+    min_qty: 6,
+    max_qty: 6
+}, {
+    _id: 'e_1000005',
+    name: '杂物线',
+    type: 'cc',
+    item_types: [{
+         _id: 'e_1000005_1',
+        name: '保健品'
+    }, {
+         _id: 'e_1000005_2',
+        name: '化妆品',
+        max_qty: 2 
+    }, {
+         _id: 'e_1000005_3',
+        name: '食品',
+        max_qty: 3
+    }],
+    rate: 6,
+    min_weight: 1000,
+    max_weight: 3500,
+    max_value: 200,
+    max_qty: 8
+}, {
+    _id: 'e_1000006',
+    name: '首饰线',
+    type: 'cc',
+    item_types: [{
+        _id: 'e_1000006_1',
+        name: '首饰'
+    }],
+    rate: 6,
+    max_weight: 1000,
+    max_value: 200,
+    max_qty: 3
+}, {
+    _id: 'e_1000007',
+    name: '固态食品线',
+    type: 'bc',
+    item_types: [{
+        _id: 'e_1000007_1',
+        name: '食品'
+    }],
+    rate: 8,
+    max_weight: 1000,
+    max_value: 200,
+    max_qty: 3
+}];
+
+module.exports = { costFirstService, speedFirstService };
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const Distribution = require('../lib');
 const should = require('chai').should(); // jshint ignore:line
-const services = require('./config').services;
+const costFirstService = require('./config').costFirstService;
+const speedFirstService = require('./config').speedFirstService;
 
 describe('费用优先 - 分包', () => {
     it('能够根据数量上线分包, 包裹重量包含包裹自身重量',  () => {
@@ -12,7 +13,7 @@ describe('费用优先 - 分包', () => {
             value: 8,
             qty: 14
         }];
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(costFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('money');
         // 总费用
         cost.should.equal(67.2); 
@@ -52,7 +53,7 @@ describe('费用优先 - 分包', () => {
             value: 15,
             qty: 2,
         }];
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(costFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('money');
         
         // 总费用
@@ -87,7 +88,7 @@ describe('费用优先 - 分包', () => {
             value: 100,
             qty: 3
         }];
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(costFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('money');
 
         // 总费用
@@ -128,7 +129,7 @@ describe('费用优先 - 分包', () => {
             qty: 3
         }];
 
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(costFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('money');
         // 总费用
         cost.should.equal(29.1);
@@ -167,7 +168,7 @@ describe('费用优先 - 分包', () => {
             qty: 3
         }];
 
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(costFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('money');
         
         // 总费用
@@ -212,7 +213,7 @@ describe('费用优先 - 分包', () => {
             value: 20,
             qty: 2
         }];
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(costFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('money');
         
         // 使用一个定量包
@@ -240,8 +241,8 @@ describe('费用优先 - 分包', () => {
     });
 });
 
-describe('速冻优先 - 分包', () => {
-    it('能够根据数量上线分包, 包裹重量包含包裹自身重量',  () => {
+describe('速度优先 - 分包', () => {
+    it('能够找到速度优先的分包方案, 即使费用高',  () => {
         const order = [{
             _id: 'p_1000001',
             name: '德运奶粉',
@@ -250,106 +251,65 @@ describe('速冻优先 - 分包', () => {
             value: 8,
             qty: 14
         }];
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(speedFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('speed');
+
         // 总费用
         cost.should.equal(67.2); 
         // 使用一个快递服务
         Object.keys(plan).length.should.equal(1);
-        plan.should.have.property('e_1000001');
+        plan.should.have.property('e_1000011');
         // 打两个包
-        plan.e_1000001.length.should.equal(2);
+        plan.e_1000011.length.should.equal(2);
         // 第1个包的信息
-        plan.e_1000001[0].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
-        plan.e_1000001[0].item[0].name.should.equal(order[0].name);
-        plan.e_1000001[0].item[0]._id.should.equal(order[0]._id);
-        plan.e_1000001[0].item[0].qty.should.equal(8);
-        plan.e_1000001[0].weight.should.equal(10900);
-        plan.e_1000001[0].postage.should.equal(38.15);
+        plan.e_1000011[0].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
+        plan.e_1000011[0].item[0].name.should.equal(order[0].name);
+        plan.e_1000011[0].item[0]._id.should.equal(order[0]._id);
+        plan.e_1000011[0].item[0].qty.should.equal(8);
+        plan.e_1000011[0].weight.should.equal(10900);
+        plan.e_1000011[0].postage.should.equal(38.15);
         // 第2个包的信息
-        plan.e_1000001[1].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
-        plan.e_1000001[1].item[0].name.should.equal(order[0].name);
-        plan.e_1000001[1].item[0]._id.should.equal(order[0]._id);
-        plan.e_1000001[1].item[0].qty.should.equal(6);
-        plan.e_1000001[1].weight.should.equal(8300);
-        plan.e_1000001[1].postage.should.equal(29.05);
+        plan.e_1000011[1].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
+        plan.e_1000011[1].item[0].name.should.equal(order[0].name);
+        plan.e_1000011[1].item[0]._id.should.equal(order[0]._id);
+        plan.e_1000011[1].item[0].qty.should.equal(6);
+        plan.e_1000011[1].weight.should.equal(8300);
+        plan.e_1000011[1].postage.should.equal(29.05);
     });
-    it('能区分杂物与成人奶粉', () => {
+    it('能够在速度相同的时候选择费用最少的分包方案(1个商品)', () => {
         const order = [{
-            _id: 'p_1000001',
-            name: '德运奶粉',
+            _id: 'p_1000006',
+            name: '爱他美2段',
             weight: 1300,
-            type: '成人奶粉',
-            value: 8,
-            qty: 6
-        }, {
-            _id: 'p_1000003',
-            name: 'Swisse维生素',
-            weight: 300,
-            type: '保健品',
-            value: 15,
-            qty: 2,
+            type: '婴儿奶粉',
+            value: 40,
+            qty: 5
         }];
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(speedFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('speed');
-        
         // 总费用
-        cost.should.equal(35.6);
+        cost.should.equal(25.9);
         Object.keys(plan).length.should.equal(2);
-        // 奶粉经济线 - 成人 1 个包裹
-        plan.should.have.property('e_1000001');
-        plan.e_1000001.length.should.equal(1);
-        plan.e_1000001[0].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
-        plan.e_1000001[0].item[0].name.should.equal(order[0].name);
-        plan.e_1000001[0].item[0]._id.should.equal(order[0]._id);
-        plan.e_1000001[0].item[0].qty.should.equal(6);
-        plan.e_1000001[0].weight.should.equal(8300);
-        plan.e_1000001[0].postage.should.equal(29.05);
-        // 杂物线 1 个包裹
-        plan.should.have.property('e_1000005');
-        plan.e_1000005.length.should.equal(1);
-        plan.e_1000005[0].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
-        plan.e_1000005[0].item[0].name.should.equal(order[1].name);
-        plan.e_1000005[0].item[0]._id.should.equal(order[1]._id);
-        plan.e_1000005[0].item[0].qty.should.equal(2);
-        plan.e_1000005[0].weight.should.equal(1100);
-        plan.e_1000005[0].postage.should.equal(6.6);
+        // 奶粉经济线 - 婴儿 - 成人 1 个包裹
+        plan.should.have.property('e_1000002');
+        plan.e_1000002.length.should.equal(1);
+        plan.e_1000002[0].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
+        plan.e_1000002[0].item[0].name.should.equal(order[0].name);
+        plan.e_1000002[0].item[0]._id.should.equal(order[0]._id);
+        plan.e_1000002[0].item[0].qty.should.equal(2);
+        plan.e_1000002[0].weight.should.equal(3100);
+        plan.e_1000002[0].postage.should.equal(10.85);
+        // 奶粉BC线 - 3罐 1 个包裹
+        plan.should.have.property('e_1000003');
+        plan.e_1000003.length.should.equal(1);
+        plan.e_1000003[0].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
+        plan.e_1000003[0].item[0].name.should.equal(order[0].name);
+        plan.e_1000003[0].item[0]._id.should.equal(order[0]._id);
+        plan.e_1000003[0].item[0].qty.should.equal(3);
+        plan.e_1000003[0].weight.should.equal(4400);
+        plan.e_1000003[0].postage.should.equal(15);
     });
-
-    it('能够识别包裹金额上限', () => {
-        const order = [{
-            _id: 'p_1000004',
-            name: '月光宝盒',
-            weight: 200,
-            type: '化妆品',
-            value: 100,
-            qty: 3
-        }];
-        const distrubution = new Distribution(services, order);
-        const { plan, cost } = distrubution.showMeTheAnswer('speed');
-
-        // 总费用
-        cost.should.equal(12);
-        // 使用1个快递
-        plan.should.have.property('e_1000005');
-        // 两个包裹
-        plan.e_1000005.length.should.equal(2);
-        // 第一个包裹
-        plan.e_1000005[0].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
-        plan.e_1000005[0].item[0].name.should.equal(order[0].name);
-        plan.e_1000005[0].item[0]._id.should.equal(order[0]._id);
-        plan.e_1000005[0].item[0].qty.should.equal(2);
-        plan.e_1000005[0].weight.should.equal(900);
-        plan.e_1000005[0].postage.should.equal(6);
-        // 第二个包裹
-        plan.e_1000005[1].item.length.should.equal(1); // 第一个包裹里只有一个种类的商品
-        plan.e_1000005[1].item[0].name.should.equal(order[0].name);
-        plan.e_1000005[1].item[0]._id.should.equal(order[0]._id);
-        plan.e_1000005[1].item[0].qty.should.equal(1);
-        plan.e_1000005[1].weight.should.equal(700); // TODO 是最小重量还是实际重量
-        plan.e_1000005[1].postage.should.equal(6);
-    });
-    it('能够给出婴儿奶粉的组合', () => {
+    it('能够在速度相同的时候选择费用最少的分包方案(2个商品)', () => {
         const order = [{
             _id: 'p_1000005',
             name: '爱他美1段',
@@ -366,114 +326,26 @@ describe('速冻优先 - 分包', () => {
             qty: 3
         }];
 
-        const distrubution = new Distribution(services, order);
+        const distrubution = new Distribution(speedFirstService, order);
         const { plan, cost } = distrubution.showMeTheAnswer('speed');
         // 总费用
-        cost.should.equal(29.1);
+        cost.should.equal(30);
         // 使用1个快递
         Object.keys(plan).length.should.equal(1);
         // 奶粉经济线 - 婴儿 1 个包裹 2 个商品
-        plan.should.have.property('e_1000002');
-        plan.e_1000002.length.should.equal(1);
-        plan.e_1000002[0].postage.should.equal(29.05);
-        plan.e_1000002[0].item.length.should.equal(2);
-        plan.e_1000002[0].weight.should.equal(8300);
+        plan.should.have.property('e_1000003');
+        plan.e_1000003.length.should.equal(2);
         // 第1个商品
-        plan.e_1000002[0].item[0].name.should.equal(order[0].name);
-        plan.e_1000002[0].item[0]._id.should.equal(order[0]._id);
-        plan.e_1000002[0].item[0].qty.should.equal(3);
+        plan.e_1000003[0].item[0].name.should.equal(order[0].name);
+        plan.e_1000003[0].item[0]._id.should.equal(order[0]._id);
+        plan.e_1000003[0].item[0].qty.should.equal(3);
+        plan.e_1000003[0].postage.should.equal(15);
+        plan.e_1000003[0].weight.should.equal(4400);
         // 第2个商品
-        plan.e_1000002[0].item[1].name.should.equal(order[1].name);
-        plan.e_1000002[0].item[1]._id.should.equal(order[1]._id);
-        plan.e_1000002[0].item[1].qty.should.equal(3);
-    });
-
-    it('能够正确使用定量包(min_qty & max_qty)', () => {
-        const order = [{
-            _id: 'p_1000005',
-            name: '爱他美1段',
-            weight: 1800,
-            type: '婴儿奶粉',
-            value: 30,
-            qty: 3,
-        }, {
-            _id: 'p_1000006',
-            name: '爱他美2段',
-            weight: 1800,
-            type: '婴儿奶粉',
-            value: 40,
-            qty: 3
-        }];
-
-        const distrubution = new Distribution(services, order);
-        const { plan, cost } = distrubution.showMeTheAnswer('speed');
-        
-        // 总费用
-        cost.should.equal(30);
-        // 使用1个快递
-        Object.keys(plan).length.should.equal(1);
-        // 奶粉BC线 1 定量包裹
-        plan.should.have.property('e_1000004');
-        plan.e_1000004.length.should.equal(1);
-        plan.e_1000004[0].postage.should.equal(30);
-        plan.e_1000004[0].weight.should.equal(11300);
-        plan.e_1000004[0].item.length.should.equal(2);
-        // 第1个商品
-        plan.e_1000004[0].item[0].name.should.equal(order[0].name);
-        plan.e_1000004[0].item[0]._id.should.equal(order[0]._id);
-        plan.e_1000004[0].item[0].qty.should.equal(3);
-        // 第2个商品
-        plan.e_1000004[0].item[1].name.should.equal(order[1].name);
-        plan.e_1000004[0].item[1]._id.should.equal(order[1]._id);
-        plan.e_1000004[0].item[1].qty.should.equal(3);
-    });
-    it('能够使用定量包,混装多个商品', () => {
-        const order = [{
-            _id: 'p_1000005',
-            name: '爱他美1段',
-            weight: 10800,
-            type: '婴儿奶粉',
-            value: 50,
-            qty: 2,
-        }, {
-            _id: 'p_1000006',
-            name: '爱他美2段',
-            weight: 10800,
-            type: '婴儿奶粉',
-            value: 30,
-            qty: 2
-        }, {
-            _id: 'p_1000007',
-            name: '爱他美3段',
-            weight: 10800,
-            type: '婴儿奶粉',
-            value: 20,
-            qty: 2
-        }];
-        const distrubution = new Distribution(services, order);
-        const { plan, cost } = distrubution.showMeTheAnswer('speed');
-        
-        // 使用一个定量包
-        cost.should.equal(30);
-        // 使用一个快递 
-        Object.keys(plan).length.should.equal(1);
-        // 一个定量包奶粉BC线 - 6罐
-        plan.should.have.property('e_1000004');
-        plan.e_1000004.length.should.equal(1);
-        plan.e_1000004[0].postage.should.equal(30);
-        plan.e_1000004[0].weight.should.equal(65300);
-        plan.e_1000004[0].item.length.should.equal(3);
-        // 第1个商品
-        plan.e_1000004[0].item[0].name.should.equal(order[0].name);
-        plan.e_1000004[0].item[0]._id.should.equal(order[0]._id);
-        plan.e_1000004[0].item[0].qty.should.equal(2);
-        // 第2个商品
-        plan.e_1000004[0].item[1].name.should.equal(order[1].name);
-        plan.e_1000004[0].item[1]._id.should.equal(order[1]._id);
-        plan.e_1000004[0].item[1].qty.should.equal(2);
-        // 第3个商品
-        plan.e_1000004[0].item[2].name.should.equal(order[2].name);
-        plan.e_1000004[0].item[2]._id.should.equal(order[2]._id);
-        plan.e_1000004[0].item[2].qty.should.equal(2);
+        plan.e_1000003[1].item[0].name.should.equal(order[1].name);
+        plan.e_1000003[1].item[0]._id.should.equal(order[1]._id);
+        plan.e_1000003[1].item[0].qty.should.equal(3);
+        plan.e_1000003[1].postage.should.equal(15);
+        plan.e_1000003[1].weight.should.equal(4400);
     });
 });
